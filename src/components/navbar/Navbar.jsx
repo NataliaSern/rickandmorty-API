bf7a114afceb364b274af248f3b3258fd05d1ce6
Natalia Serna <nataliasernacast@gmail.com>
2021-02-18 08:17:06 -0500

    ok

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -9,8 +9,8 @@ export default class Navbar extends Component {
         <div className="container-fluid">
           <Link className="Navbar__brand" to="/">
             <img className="Navbar__brand-logo" width="60" height="60" style={{borderRadius: '50%'}} src="https://i.pinimg.com/736x/f6/ba/96/f6ba9626bc761bc5408da7c4a4983afa.jpg" alt="Logo" />
-            <span className="font-weight-bold">Rick & Morty &nbsp;</span>
-            <span className="font-weight-light"> API Characters </span>
+            <span className="font-weight-bold" style={{color: "#333"}}>Rick & Morty &nbsp;</span>
+            <span className="font-weight-light"  style={{color: "#000"}}> API Characters </span>
           </Link>
         </div>
       </div>
